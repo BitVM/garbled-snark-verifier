3e691d7cd34be2c2ce48e7a2b8984a79e5ef7150
cyphersnake <cyphersnake@pm.me>
2025-09-16 12:48:28 +0200

    perf(example): compressed with single-thread hash

--- examples/groth16_garble_with_compress.rs
+++ examples/groth16_garble_with_compress.rs
@@ -101,19 +101,7 @@ fn run_with_hasher<H: GateHasher + 'static>(garbling_seed: u64) {
     }
     .compress();
 
-    // Create channel for garbled tables
-    let (ciphertext_acc_hash_sender, ciphertext_acc_hash_receiver) =
-        crossbeam::channel::unbounded();
-
-    let ciphertext_hash = thread::spawn(move || {
-        info!("Starting ciphertext hashing thread...");
-
-        let mut hasher = CiphertextHashAcc::default();
-        while let Ok((_index, ciphertext)) = ciphertext_acc_hash_receiver.recv() {
-            hasher.update(ciphertext)
-        }
-        hasher.finalize()
-    });
+    let hasher = CiphertextHashAcc::default();
 
     info!("Starting garbling of Groth16 verification circuit...");
 
@@ -125,7 +113,7 @@ fn run_with_hasher<H: GateHasher + 'static>(garbling_seed: u64) {
             inputs.clone(),
             CAPACITY,
             garbling_seed,
-            ciphertext_acc_hash_sender,
+            hasher,
             garbled_groth16::verify_compressed,
         );
 
@@ -135,7 +123,7 @@ fn run_with_hasher<H: GateHasher + 'static>(garbling_seed: u64) {
     let GarbledWire { label0, label1 } = *garbling_result.output_labels();
     let input_values = garbling_result.input_wire_values;
 
-    let ciphertext_hash: u128 = ciphertext_hash.join().unwrap();
+    let ciphertext_hash: u128 = garbling_result.ciphertext_handler_result;
 
     // NOTE For the SetupPhase, we must use a random set of bytes and compare
     // them with the hash provided earlier.
