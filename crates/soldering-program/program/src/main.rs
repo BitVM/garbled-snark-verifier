8e5805d499a4a423eba5c6f09a26efad9e3b060b
cyphersnake <cyphersnake@pm.me>
2025-10-08 13:41:36 +0200

    wip: feat: sha256 for sp1-program

--- crates/soldering-program/program/src/main.rs
+++ crates/soldering-program/program/src/main.rs
@@ -1,9 +1,11 @@
 #![no_main]
 sp1_zkvm::entrypoint!(main);
 
+use sha2::Digest;
 use soldering_types::Input;
 
 pub fn main() {
     let input = sp1_zkvm::io::read::<Input>();
+    let _ = sha2::Sha256::digest(input.private_param.labels[0].labels[0].label0);
     sp1_zkvm::io::commit(&input.public_param);
 }
